a4e954206266d21d4aad1f6b555f3ca46c043b17
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-04-05 13:17:44 +0200

    searchField addet.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-     monsters: []
+     monsters: [],
+     searchField:''
     };
   }
 
@@ -21,6 +22,9 @@ class App extends Component {
   render() {
     return (
       <div className='App'> 
+        <input type='search' placeholder='Search here!' 
+          onChange={e => this.setState({ searchField: e.target.value})}
+        />
         <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
